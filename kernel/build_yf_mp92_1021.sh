cf0e886b2af0c859dddb2fe00efdc7cf82f8ef1c
andrey <andrey@andrey-HP-Z400-Workstation.(none)>
2015-06-10 09:52:29 +0300

    build script for Prestigio pmp5101

---
+++ kernel/build_yf_mp92_1021.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PATH=~/kernel_build/arm-eabi-4.6/bin/:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=~/kernel_build/arm-eabi-4.6/bin/arm-eabi-
+export LOCALVERSION=+
+#export INSTALL_MOD_PATH=~/kernel_build/rk3188/mod_fw
+
+#board name
+./mk.sh "mp92_1021"
+
+
